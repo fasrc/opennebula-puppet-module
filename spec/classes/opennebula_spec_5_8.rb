a51ad37b3a6bcd155c316c3ebea6cb5cca9f27ff
greyspectrum <austin_smith@harvard.edu>
2019-09-24 16:25:06 -0400

    add tests for ONE 5.8

---
+++ spec/classes/opennebula_spec_5_8.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+hiera_config = 'spec/fixtures/hiera/hiera.yaml'
+
+describe 'one', :type => :class do
+  let(:hiera_config) { hiera_config }
+  OS_FACTS.each do |f|
+    context "On #{f[:operatingsystem]} #{f[:operatingsystemmajrelease]}" do
+      let(:facts) { f }
+      hiera = Hiera.new(:config => hiera_config)
+      configdir = '/etc/one'
+      oned_config = "#{configdir}/oned.conf"
+      context 'as oned-5.8 with default params' do
+        let(:params) { {
+            :oned => true,
+            :one_version => '5.8',
+            :node => false,
+        } }
+        it { should contain_file(oned_config).with_content(/^MONITORING_INTERVAL_HOST\s+= 180/m) }
+        it { should contain_file(oned_config).with_content(/^MONITORING_INTERVAL_VM\s+= 180/m) }
+        it { should contain_file(oned_config).with_content(/^MONITORING_INTERVAL_DATASTORE\s+= 300/m) }
+        it { should contain_file(oned_config).with_content(/^MONITORING_INTERVAL_MARKET\s+= 600/m) }
+      end
+      context 'as oned-5.8 with custom params' do
+        let(:params) { {
+            :oned => true,
+            :one_version => '5.8',
+            :node => false,
+            :monitoring_interval_host => '200',
+            :monitoring_interval_vm => '200',
+            :monitoring_interval_datastore => '400',
+            :monitoring_interval_market => '800',
+        } }
+        it { should contain_file(oned_config).with_content(/^MONITORING_INTERVAL_HOST\s+= 200/m) }
+        it { should contain_file(oned_config).with_content(/^MONITORING_INTERVAL_VM\s+= 200/m) }
+        it { should contain_file(oned_config).with_content(/^MONITORING_INTERVAL_DATASTORE\s+= 400/m) }
+        it { should contain_file(oned_config).with_content(/^MONITORING_INTERVAL_MARKET\s+= 800/m) }
+      end
+    end
+  end
+end
